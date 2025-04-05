9b506e634b9c3780de80723cdd0f4cb4d7087ffc
Diya Goyal <dgoyal6_be22@thapar.edu>
2025-04-05 11:34:10 +0530

    flutter

---
+++ lib/pages/home.dart
@@ -0,0 +1,29 @@
+ import 'package:flutter/material.dart';
+//stl-> stateless widget
+
+ class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Breakfast'),
+        titleTextStyle:TextStyle(
+          color: Colors.black,
+          fontSize: 18,
+          fontWeight: FontWeight.bold,
+        ),
+        backgroundColor: Colors.white,
+        elevation: 0,
+        centerTitle: true,
+        leading:Container(
+          decoration: BoxDecoration(
+            color: Colors.black,
+            borderRadius: BorderRadius.circular(10)
+          ),
+        ) ,
+      ),
+    );
+  }
+}
